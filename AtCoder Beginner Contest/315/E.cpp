27377fdef245f9b14f9827c291fa064f80c3836a
sukenori <sukenori@coo.net>
2023-08-20 20:35:33 +0900

    Daily diligence

--- AtCoder Beginner Contest/315/E.cpp
+++ AtCoder Beginner Contest/315/E.cpp
@@ -8,12 +8,10 @@ int main(){
   int C; for(int i=1;i<=N;i++){cin>>C; int P;
     for(int j=0;j<C;j++){cin>>P; G[i].push_back(P);}
   }
-  queue<int>q; q.push(1); d[1]=1;
-  while(q.size()){
-    int i=q.front(); q.pop();
-    for(int j:G[i]){if(!d[j])q.push(j); d[j]=d[i]+1;}
-  }
-  set<pair<int,int>,greater<pair<int,int>>>s;
-  for(int i=1;i<=N;i++)s.insert({d[i],i});
-  for(auto si:s)if(si.first>1)cout<<si.second<<' ';
+  vector<int>a;
+  function<void(int)>dfs=[&](int i){
+    for(int j:G[i])if(!d[j])dfs(j);
+    d[i]=1; if(i>1)cout<<i<<' ';
+  };
+  dfs(1);
 }
