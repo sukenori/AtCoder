52f9bd1f298790a97be371547a61f5b52183fee0
sukenori <sukenori@coo.net>
2023-08-31 23:31:44 +0900

    Daily diligence

--- Competitive Programming Typical 90 Questions/016 - Minimum Coins.cpp
+++ Competitive Programming Typical 90 Questions/016 - Minimum Coins.cpp
@@ -1,11 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
-int N,c[3];
+int N,A,B,C;
 int main(){
-  cin>>N>>c[0]>>c[1]>>c[2];
-  sort(c,c+3);
-  int a=0; for(int i=2;i>=0;i--){
-    a+=N/c[i]; N%=c[i];
+  cin>>N>>A>>B>>C;
+  int a=10000;
+  for(int i=0;i<=min(N/A,9999);i++)for(int j=0;j<=min(N/B,9999);j++){
+      int r=N-A*i-B*j; if(r>=0&&r%C==0)a=min(a,i+j+r/C);
   }
   cout<<a<<endl;
 }
