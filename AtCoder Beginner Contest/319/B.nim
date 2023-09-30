2cc15f83f17534b46cec665019838da3e4b51e34
sukenori <sukenori@coo.net>
2023-09-30 20:42:46 +0900

    Daily diligence

--- AtCoder Beginner Contest/319/B.nim
+++ AtCoder Beginner Contest/319/B.nim
@@ -1,8 +1,8 @@
-include atcoder/header
+include atcoder/extra/header/chaemon_header
 let N=nextInt()
 var a="-".repeat(N+1)
 for i in 0..N:
   var s=10
   for j in 9.countdown(1):
-    if N mod j==0 and i mod (N div j)==0: a[i]=($j)[0]
+    if N%j==0 and i%(N//j)==0: a[i]=($j)[0]
 echo a
--- AtCoder Beginner Contest/319/E.nim
+++ AtCoder Beginner Contest/319/E.nim
@@ -2,7 +2,15 @@ include atcoder/header
 let
   N,X,Y=nextInt()
   b=newSeqWith(N-1,(P:nextInt(),T:nextInt()))
-var
+  n=(1..8).toSeq.foldl(lcm(a,b))
   Q=nextInt()
   q=newSeqWith(Q,nextInt())
-echo b[2].T
+var a=newSeq[int](n)
+for i in 0..<n:
+  a[i]=i
+  for j in 0..<N-1:
+    var m=a[i] mod b[j].P
+    a[i]+=(if m>0: b[j].P-m else: 0)+b[j].T
+for qi in q:
+  var s=qi+X
+  echo s-s mod n+a[s mod n]+Y
