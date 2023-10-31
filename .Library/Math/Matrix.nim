6bdeb5fe56f89a8dd7539ca074a7cdc5d4ca76cd
sukenori <sukenori@coo.net>
2023-10-31 22:56:53 +0900

    ABC326 held

--- .Library/Math/Matrix.nim
+++ .Library/Math/Matrix.nim
@@ -1,3 +1,3 @@
 #Transpose
 (block: collect(newSeq):
-  for i in 0..n<<1: 2Dseq.mapIt(it[i]))
+  for i in 0..<N<<1: 2Dseq.mapIt(it[i]))
--- Recommendation.nim
+++ Recommendation.nim
@@ -1,9 +1,15 @@
 include atcoder/extra/header/chaemon_header
-t:=Seq[seq[int]]
-proc f(i:int)=
-  if i==3: echo t
-  else:
-    for j in 0..<t.len:
-      t[j].add(i); f(i+1); discard t[j].pop
-    t.add(@[i]); f(i+1); discard t.pop   
-f(0)
+N:=nextInt()
+let R,C=nextString()
+t:=newTable[char,seq[string]]()
+a:=".".repeat(N-3)&"ABC"
+while true:
+  h:=a.replace(".")[0]
+  if t.hasKey(h): t[h].add(a) else: t[h]= @[a]
+  if not a.nextPermutation(): break
+A:=Seq[seq[string]]
+for Ri in R: A.add(t[Ri])
+for Ai in product(A):
+  At:=(block: collect(newSeq):
+    for j in 0..<N<<1: Ai.mapIt(it[j]))
+  echo At.join
