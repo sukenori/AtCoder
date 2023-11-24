a9bdcd10cade2114075bfcd2f098a9eaea049c62
sukenori <sukenori@coo.net>
2023-11-24 20:39:43 +0900

    ABC329 held

--- AtCoder Beginner Contest/327/F.nim
+++ AtCoder Beginner Contest/327/F.nim
@@ -1,30 +1,24 @@
 include atcoder/extra/header/chaemon_header
 let N,D,W=nextInt()
-A:=Seq[2*10^5: @int]
-mT:=0; mX:=0
-for _ in 1..N:
-  let T,X=nextInt()-1; A[T].add(X)
-  mT.max=T; mX.max=X
-echo A[0..6]
-n:=1; while n<mX+1: n*=2
-var s=Seq[2*n-1:0]
-a:=0
-for t in 0..mT:
-  proc p(a,b,x,k,l,r:int)=
-    if r<=a or b<=l: return    
-    elif a<=l and r<=b:
-      s[k]+=x
-      i:=k
-      while i>0: s[(i-1)//2].max=s[i]; i=(i-1)//2
-      proc c(j:int)=
-        if j<n-1:
-          s[2*j+1]+=x; c(2*j+1)
-          s[2*j+2]+=x; c(2*j+2)
-      c(k)
-    else: p(a,b,x,2*k+1,l,(l+r)//2); p(a,b,x,2*k+2,(l+r)//2,r)
-  for x in A[t]: p(max(0,x-(W-1)),min(x+(W-1),mX),1,0,0,n)
-  if t-(D-1)>=0:
-    for x in A[t-(D-1)]: p(max(0,x-(W-1)),min(x+(W-1),mX),-1,0,0,n)
-  a.max=s[0]
-  echo s
+import atcoder/lazysegtree
+type
+  S=int
+  F=int
+proc op(a,b:S):S=max(a,b)
+proc e():S=0
+proc mapping(f:F,x:S):S=x+f
+proc composition(f,g:F):F=f+g
+proc id():F=0
+n:=2*10^5
+s:=LazySegTree.getType(S,F,op,e,mapping,composition,id).init(n)
+A:=Seq[N:tuple[T,X:int]]
+for i in 0..<N: A[i]=(nextInt(),nextInt())
+A.sort
+j:=0; a:=0
+for i in 0..<N:
+  s.apply(A[i].X..<min(A[i].X+W,n),1)
+  while j<N and A[j].T<=A[i].T-D:
+    s.apply(A[j].X..<min(A[j].X+W,n),-1)
+    j+=1
+  a.max=s.all_prod()
 echo a
