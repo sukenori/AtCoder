000883647e15ee58de2aa9d2e60dee3ee32a8ca6
sukenori <sukenori@coo.net>
2024-02-04 10:08:15 +0900

    ABC339 held

--- .Library/Others/Direction.nim
+++ .Library/Others/Direction.nim
@@ -1,6 +1,10 @@
 import atcoder/extra/other/direction
 for (ni,nj) in (i,j).neighbor(dir4,(0..<H,0..<W)): #dir8
 
+di:=[-1,0,1,0]; dj:=[0,1,0,-1]
+
+for (di,dj) in [(-1,0),(0,1),(1,0),(0,-1)]:
+
 #Sentinel
 m:=Seq[H:"#"&nextString()&"#"]
 for i in [0,H+1]: m.insert("#".repeat(W+1),i)
