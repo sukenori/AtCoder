6ae64a3b497c71060a19b085cb370c2095ca7e92
sukenori <sukenori@coo.net>
2024-04-13 20:30:50 +0900

    ABC348 held

--- .Library/Others/Direction.nim
+++ .Library/Others/Direction.nim
@@ -1,12 +1,15 @@
 import atcoder/extra/other/direction
 for (ni,nj) in (i,j).neighbor(dir4,(0..<H,0..<W)): #dir8
 
-let
-  di=[-1,0,1,0]
-  dj=[0,1,0,-1]
+let (di,dj)=([-1,0,1,0],[0,1,0,-1])
+for i in 0..<4:
+  let (ni,nj)=(i+di[i],j+dj[i])
+  if ni in 0..<H and nj in 0..<W and   
 
 for (di,dj) in [(-1,0),(0,1),(1,0),(0,-1)]:
+  let (ni,nj)=(i+di,j+dj)
+  if ni in 0..<H and nj in 0..<W and 
 
 #Sentinel
 let m=newSeqWith(H,"#"&nextString()&"#")
-for i in [0,H+1]: m.insert("#".repeat(W+1),i)
+for i in [0,H+1]: m.insert("#".repeat(W+2),i)
