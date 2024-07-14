5cd3cf96eebdc23a5979712708f7c2d47420885e
sukenori <sukenori@coo.net>
2024-07-14 09:14:22 +0900

    ABC362 held

--- .Library/Graph/Dijkstra.nim
+++ .Library/Graph/Dijkstra.nim
@@ -1,9 +1,14 @@
 import heapqueue
-var q=[(d:0,i:0)].toHeapQueue
-var d=newSeqWith(N,int.inf); d[0]=0
+var
+  q=[(w:0,i:0)].toHeapQueue
+  w=newSeqWith(N,int.inf)
+  d=newSeqWith(N,false)
+w[0]=0
 while q.len>0:
   let i=q.pop
-  for j in g[i.i]:
-    let nw=i.d+j.w
-    if nw<d[j.t]: d[j.t]=nw; q.push((nw,j.t))
-#echo d[^1]
+  if not d[i.i]:
+    d[i.i]=true
+    for j in g[i.i]:
+      let nw=i.w+j.w
+      if nw<w[j.t]: w[j.t]=nw; q.push((nw,j.t))
+#echo w[^1]
